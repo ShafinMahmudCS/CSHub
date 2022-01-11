0168a7481c5ece8c8b1fe0816f6546d8afacf8db
ShafinMahmudCS <shafin22@my.yorku.ca>
2022-01-11 18:51:53 -0500

    Toggle bar added.

--- Script/Home.js
+++ Script/Home.js
@@ -1,16 +1,25 @@
-window.onload = function() {
-    slideShow();
-  };
+// window.onload = function() {
+//     slideShow();
+//   };
 
-var index = 0;
-function slideShow() {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
-  }
-  index++;
-  if (index > slides.length) {index = 1}    
-  slides[index-1].style.display = "block";  
-  setTimeout(slideShow, 2000); 
+// var index = 0;
+// function slideShow() {
+//   var i;
+//   var slides = document.getElementsByClassName("mySlides");
+//   for (i = 0; i < slides.length; i++) {
+//     slides[i].style.display = "none";  
+//   }
+//   index++;
+//   if (index > slides.length) {index = 1}    
+//   slides[index-1].style.display = "block";  
+//   setTimeout(slideShow, 2000); 
+// }
+
+function classToggle() {
+  const navs = document.querySelectorAll('.Navbar__Items')
+  
+  navs.forEach(nav => nav.classList.toggle('Navbar__ToggleShow'));
 }
+
+document.querySelector('.Navbar__Link-toggle')
+  .addEventListener('click', classToggle);
