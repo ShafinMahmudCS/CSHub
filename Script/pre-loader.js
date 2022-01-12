4bd61b94ebc238d8880ef04702be8538ccfa3977
ShafinMahmudCS <shafin22@my.yorku.ca>
2022-01-11 21:38:26 -0500

    Dead code deleted.

--- Script/pre-loader.js
+++
@@ -1,5 +0,0 @@
-var loader = setInterval(function () {
-    if(document.readyState !== "complete") return;
-    clearInterval(loader);
-    document.querySelector('.spinner-wrapper').style.display = "none";
-  }, 450);
